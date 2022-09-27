1b855dc6c96f2d23d1d03737022cf662534ca46a
Salz <74233105+salzteam@users.noreply.github.com>
2022-09-27 10:43:12 +0700

    Update "Data not Found!"

--- searchName.js
+++ searchName.js
@@ -2,7 +2,7 @@ function searchName(input, limits, transformationFunction) {
   if (typeof input !== "string") return "Invalid, Name must be string!";
   if (typeof limits !== "number")
     return "Invalid, Limit output must be number!";
-  if (typeof transformationFunction !== "function") return "Invalid Function";
+  if (typeof transformationFunction !== "function") return "Invalid Function!";
   const name = [
     "Abigail",
     "Alexandra",
@@ -27,6 +27,7 @@ function searchName(input, limits, transformationFunction) {
     // console.log(resultSearch);
     // console.log(typeof resultSearch);
   }
+  if (resultSearch.length == 0) return "Data not found!";
   if (resultSearch.length >= 0) return resultSearch.slice(0, limits);
 }
 
